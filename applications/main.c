5faad12cd306bed77df9dd4da8644e37371f2a57
LitchiCheng <darboy@foxmail.com>
2021-12-05 23:07:43 +0800

    feat:增加计算pitch

--- applications/main.c
+++ applications/main.c
@@ -2,6 +2,7 @@
 #include <rtthread.h>
 #include "math.h"
 #include "drv_mpu6050.h"
+#include "stdbool.h"
  
 void mpu6050_thread_entry(void *parameter)
 {
@@ -11,6 +12,8 @@ void mpu6050_thread_entry(void *parameter)
     
     rt_err_t ret;
     double angle = 0;
+		rt_tick_t last_tick = rt_tick_get();
+		bool first_in = true;
     while(1)
     {
         ret = mpu6050_temperature_get(&temp);
@@ -22,25 +25,31 @@ void mpu6050_thread_entry(void *parameter)
         if (ret != RT_EOK)
         {
             rt_kprintf("mpu6050 : get acc error\r\n");
-        } 
+        }
         ret = mpu6050_gyroscope_get(&gx, &gy, &gz);
         if (ret != RT_EOK)
         {
             rt_kprintf("mpu6050 : get gyro error\r\n");
-        } 
+        }else{
+					
+					if(first_in){
+							first_in = false;
+							last_tick = rt_tick_get();
+					}
+				} 
         if (ret == RT_EOK)
         {
             //rt_kprintf("mpu6050: temperature=%-6d gx=%-6d gy=%-6d gz=%-6d ax=%-6d ay=%-6d az=%-6d\r\n",temp/100,gx,gy,gz,ax,ay,az);
         }
-				double gz_f = gz;
-				if(fabs(gz_f) < 30){
-						gz_f = 0;
+				
+				double gy_f = gy;
+				if(fabs(gy_f) < 30){
+						gy_f = 0;
 				}
-				angle += (gz_f / 16) * 0.005;
-				uint64_t angele_k = (uint64_t)angle;
-				rt_kprintf("angle is %f\r\n", angle);
-				//char dd[20];
-				//sprintf(dd, "angle is %f\r\n", angle);
+				angle += (gy_f / 16.4) * (rt_tick_get()-last_tick / RT_TICK_PER_SECOND);
+				last_tick = rt_tick_get();
+				
+				rt_kprintf("pitch is %f angleX is %f\r\n", angle, atan2((ax / 16384.0),(az / 16384.0))*180/3.14);
         rt_thread_delay(rt_tick_from_millisecond(5));    
     }
 }
