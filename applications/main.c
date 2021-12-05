57f3cbd8cc161f6af331da4b1e8238d6ab7828a3
LitchiCheng <darboy@foxmail.com>
2021-12-05 19:37:43 +0800

    feat:增加rt_kprintf支持浮点数打印

--- applications/main.c
+++ applications/main.c
@@ -1,6 +1,6 @@
 #include <rthw.h>
 #include <rtthread.h>
- 
+#include "math.h"
 #include "drv_mpu6050.h"
  
 void mpu6050_thread_entry(void *parameter)
@@ -10,7 +10,7 @@ void mpu6050_thread_entry(void *parameter)
     rt_int16_t ax,ay,az;    //?????
     
     rt_err_t ret;
-    
+    double angle = 0;
     while(1)
     {
         ret = mpu6050_temperature_get(&temp);
@@ -30,8 +30,17 @@ void mpu6050_thread_entry(void *parameter)
         } 
         if (ret == RT_EOK)
         {
-            rt_kprintf("mpu6050: temperature=%-6d gx=%-6d gy=%-6d gz=%-6d ax=%-6d ay=%-6d az=%-6d\r\n",temp/100,gx,gy,gz,ax,ay,az);
+            //rt_kprintf("mpu6050: temperature=%-6d gx=%-6d gy=%-6d gz=%-6d ax=%-6d ay=%-6d az=%-6d\r\n",temp/100,gx,gy,gz,ax,ay,az);
         }
+				double gz_f = gz;
+				if(fabs(gz_f) < 30){
+						gz_f = 0;
+				}
+				angle += (gz_f / 16) * 0.005;
+				uint64_t angele_k = (uint64_t)angle;
+				rt_kprintf("angle is %f\r\n", angle);
+				//char dd[20];
+				//sprintf(dd, "angle is %f\r\n", angle);
         rt_thread_delay(rt_tick_from_millisecond(5));    
     }
 }
